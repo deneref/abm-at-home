3ef5f0474019bded78ad7a27e30f625851ebfd19
Даниил М <daniilmokeev13@gmail.com>
2025-08-11 14:42:57 +0300

    test commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ui.main_window import MainWindow
 
 
 def main():
-    # Инициализация базы данных и запуск приложения
+    # Инициализация базы данных и запуск приложения; 
     database.init_db()
     database.reset_all_tables()
     database.insert_data()
